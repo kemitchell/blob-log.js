1b8d10c058d574851f88ad620c0c87abe6992e58
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-02 22:48:25 -0700

    WIP: passing tests

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var Decoder = require('blob-log-decoder')
 var Encoder = require('blob-log-encoder')
-var Multistream = require('multistream')
-var PassThrough = require('readable-stream').PassThrough
-var StreamingAppend = require('stream-to-blob-log')
+var EventEmitter = require('events').EventEmitter
+var MultiStream = require('multistream')
+var MultiWritable = require('multiwritable')
+var arrayFind = require('array-find')
 var fs = require('fs')
+var inherits = require('util').inherits
 var lexint = require('lexicographic-integer')
+var mkdirp = require('mkdirp')
 var path = require('path')
 var pump = require('pump')
 var runSeries = require('run-series')
@@ -16,11 +19,7 @@ var constructorOptions = {
   blobsPerFile: optional(possiblyInfinite(positiveInteger))
 }
 
-var LOG_FILE_EXTENSION = 'bloblog'
-var LOG_FILE_RE = new RegExp('^([^.]+)\\.' + LOG_FILE_EXTENSION + '$')
-
 function BlobLog (options) {
-  // Construct a new object.
   if (!(this instanceof BlobLog)) {
     return new BlobLog(options)
   }
@@ -30,37 +29,29 @@ function BlobLog (options) {
   validateOptions(constructorOptions, options)
   self._blobsPerFile = options.blobsPerFile || 10
   self._directory = options.directory
+  self._index = null
+
+  EventEmitter.call(this)
 
   // Initialize.
   runSeries([
-    this._validateDirectory.bind(this),
+    mkdirp.bind(null, this._directory),
     this._readExistingFiles.bind(this),
-    this._checkCurrentFile.bind(this)
+    this._checkTailFile.bind(this)
   ], function (error) {
-    self.emit('error', error)
+    if (error) {
+      self.emit('error', error)
+    } else {
+      self.emit('ready')
+    }
   })
 }
+
+inherits(BlobLog, EventEmitter)
 
 var prototype = BlobLog.prototype
 
 // Initialization Methods
-
-prototype._validateDirectory = function (callback) {
-  var self = this
-  var directory = self._directory
-  // `stat` the directory path, to make sure it's a directory.
-  fs.stat(directory, function (error, stats) {
-    if (error) {
-      callback(error)
-    } else {
-      if (!stats.isDirectory()) {
-        callback(new Error('not a directory'))
-      } else {
-        callback()
-      }
-    }
-  })
-}
 
 prototype._readExistingFiles = function (callback) {
   var self = this
@@ -69,6 +60,7 @@ prototype._readExistingFiles = function (callback) {
   // and sort lexicographic-integer file names, to determine where
   // to start writing new blobs.
   fs.readdir(directory, function (error, files) {
+    /* istanbul ignore if */
     if (error) {
       callback(error)
     } else {
@@ -76,121 +68,147 @@ prototype._readExistingFiles = function (callback) {
       // correspond to existing blob log files.
       var fileNumbers = []
       files.forEach(function (file) {
-        var match = LOG_FILE_RE.exec(file)
-        if (match) {
-          var unpacked = unpackInteger(match[1])
-          if (unpacked !== undefined) {
-            fileNumbers.push(unpacked)
-          }
+        var number = logFileNumber(file)
+        if (number !== undefined) {
+          fileNumbers.push(number)
         }
       })
       fileNumbers.sort()
       // Do we have a continous set of log file numbers, without
       // any gaps?
-      var missing = fileNumbers.find(function (element, index) {
-        return element !== index - 1
+      var missing = arrayFind(fileNumbers, function (element, index) {
+        return element !== index + 1
       })
       if (missing) {
-        var missingPath = self._filePath(missing - 1)
+        var missingPath = self._filePathForIndex(missing - 1)
         callback(new Error('missing ' + missingPath))
+      } else {
+        // Store the number of the latest log file.
+        self._fileNumber = fileNumbers[fileNumbers.length - 1]
+        callback()
       }
-      // Store the numbef of the latest log file.
-      self._fileNumber = fileNumbers[fileNumbers.length - 1]
-      callback()
     }
   })
 }
 
-prototype._checkCurrentFile = function (callback) {
+prototype._checkTailFile = function (callback) {
   var self = this
-  var lastIndex = 0
-  var blobCount = 0
-  fs.createReadStream(self._currentFile())
-  .pipe(
-    new Decoder()
-    .once('error', function (error) {
-      callback(error)
+  if (self._fileNumber === undefined) {
+    self._blobsInFile = 0
+    self._index = 0
+    callback()
+  } else {
+    var lastIndex = 0
+    var blobCount = 0
+    fs.createReadStream(self._tailFilePath())
+    .once('error', /* istanbul ignore next */ function (error) {
+      if (error.code === 'ENOENT') {
+        self._blobsInFile = 0
+        self._index = 0
+        callback()
+      } else {
+        callback(error)
+      }
     })
-    .on('data', function (blob) {
-      lastIndex = blob.index
-      blobCount++
-    })
-    .once('end', function () {
-      self._blobsInFile = blobCount
-      self._index = lastIndex
-      callback()
-    })
-  )
+    .pipe(
+      new Decoder()
+      .once('error', /* istanbul ignore next */ function (error) {
+        callback(error)
+      })
+      .on('data', function (blob) {
+        lastIndex = blob.index
+        blobCount++
+      })
+      .once('end', function () {
+        self._blobsInFile = blobCount
+        self._index = lastIndex
+        callback()
+      })
+    )
+  }
 }
 
 // Log File Helper Methods
 
-prototype._filePath = function (number) {
+var LOG_FILE_EXTENSION = '.bloblog'
+
+function logFileNumber (file) {
+  var extension = path.extname(file)
+  if (extension === LOG_FILE_EXTENSION) {
+    return unpackInteger(path.basename(file, extension))
+  }
+}
+
+prototype._filePathForIndex = function (number) {
   return path.join(
     this._directory,
-    packInteger(number) + '.' + LOG_FILE_EXTENSION
+    packInteger(number) + LOG_FILE_EXTENSION
   )
 }
 
-prototype._currentFile = function () {
-  return this._filePath(this._fileNumber)
+prototype._tailFilePath = function () {
+  return this._filePathForIndex(this._fileNumber)
 }
 
-prototype._fileFor = function (index) {
+/* istanbul ignore next: TODO */
+prototype._fileNumberForIndex = function (index) {
   return Math.floor((index - 1) / this._blobsPerFile) + 1
 }
 
+/* istanbul ignore next: TODO */
 prototype._nthInFile = function (index) {
   return (index - 1) % this._blobsPerFile + 1
 }
 
+/* istanbul ignore next: TODO */
 prototype._firstInFile = function (index) {
   return this._nthInFile(index) === 1
 }
 
+/* istanbul ignore next: TODO */
 prototype._lastInFile = function (index) {
   return this._nthInFile(index) === this._blobsPerFile
 }
 
 // Public API
 
-prototype.createWriteStream = function (options) {
+prototype.createWriteStream = function () {
   var self = this
-  if (options.objectMode) {
-    var returned = new PassThrough({
-      writableObjectMode: true,
-      readableObjectMode: true
-    })
-    .prependListener('data', function () {
-      this.left--
-      if (this.left === 0) {
-        this.logFile.unpipe(this)
-        this.logFile.end()
-        var nextIndex = self._index + 1
-        this.logFile = pump(
-          new Encoder(nextIndex),
-          fs.createWriteStream(self._filePath(nextIndex))
-        )
-        this.pipe(this.logFile)
-      }
-    })
-    returned.left = Math.max(self._blobsPerFile - self._blobsInFile, 0)
-    return returned
-  } else {
+  var options = {
+    end: true,
+    objectMode: true
   }
+  function sinkFactory (currentSink, chunk, encoding, callback) {
+    var index = self._index + 1
+    var file = self._filePathForIndex(index)
+    if (currentSink && currentSink.path === file) {
+      callback(null, currentSink)
+    } else {
+      var encoder = new Encoder(index)
+      encoder.path = file
+      var writeStream = fs.createWriteStream(file)
+      pump(encoder, writeStream)
+      callback(null, encoder)
+    }
+  }
+  self._internalWriteStream = MultiWritable(sinkFactory, options)
+  return self._internalWriteStream
 }
 
 // TODO: Options object with from index
 prototype.createReadStream = function () {
   var self = this
   var fileIndex = 0
-  return new Multistream(function (callback) {
+  return MultiStream.obj(function (callback) {
     fileIndex++
     if (fileIndex > self._fileNumber) {
       callback(null, null)
     } else {
-      var readStream = fs.createReadStream(self._filePath(fileIndex))
-      callback(null, pump(readStream, new Decoder()))
+      var filePath = self._filePathForIndex(fileIndex)
+      var readStream = fs.createReadStream(filePath)
+      var decoder = new Decoder()
+      pump(readStream, decoder)
+      callback(null, decoder)
     }
   })
 }
@@ -202,7 +220,7 @@ function validateOptions (predicates, options) {
     throw new Error('missing directory')
   }
   Object.keys(predicates).forEach(function (key) {
-    if (!predicates[key](options.key)) {
+    if (!predicates[key](options[key])) {
       throw new Error('invalid ' + key)
     }
   })
